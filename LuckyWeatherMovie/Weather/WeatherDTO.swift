03ab81e3c54197923ee2d9f5b3b8c33f69c8ab16
yoruck2 <yoruck2@gmail.com>
2024-06-10 08:11:13 +0900

    WeatherDTO 추가

--- LuckyWeatherMovie/Weather/WeatherDTO.swift
+++ LuckyWeatherMovie/Weather/WeatherDTO.swift
@@ -4,5 +4,32 @@
 //
 //  Created by dopamint on 6/9/24.
 //
-
 import Foundation
+
+struct WeatherDTO: Decodable {
+    let weather: [Weather]
+    let main: Main
+    let name: String
+    let dt: Int
+}
+
+struct Main: Decodable {
+    let temp: Double
+    let pressure, humidity: Int
+    let tempMin, tempMax: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp, pressure, humidity
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+    }
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+
