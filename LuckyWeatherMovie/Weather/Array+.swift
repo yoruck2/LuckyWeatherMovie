63c238ce0eeb92dd3591b66ed3681d8a9e5fc45a
yoruck2 <yoruck2@gmail.com>
2024-06-10 08:07:53 +0900

    안전한 배열 조회를 위한 Array 익스텐션 추가

--- LuckyWeatherMovie/Weather/Array+.swift
+++ LuckyWeatherMovie/Weather/Array+.swift
@@ -5,4 +5,9 @@
 //  Created by dopamint on 6/10/24.
 //
 
-import Foundation
+extension Array {
+    
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
