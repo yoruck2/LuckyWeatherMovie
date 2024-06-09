b02873003c56bf214e98164c2c4cd6232119309c
yoruck2 <yoruck2@gmail.com>
2024-06-09 16:36:52 +0900

    Network에 getRequest 메서드 추가, LottoDTO 분리

--- LuckyWeatherMovie/Lotto/Network.swift
+++ LuckyWeatherMovie/Lotto/Network.swift
@@ -5,20 +5,12 @@
 //  Created by dopamint on 6/8/24.
 //
 
-import Foundation
 import Alamofire
 
-struct Lotto: Codable {
-    let totSellamnt: Int
-    let returnValue, drwNoDate: String
-    let firstWinamnt, firstPrzwnerCo, firstAccumamnt, drwNo: Int
-    let drwtNo1, drwtNo2, drwtNo3, drwtNo4, drwtNo5, drwtNo6, bnusNo: Int
-}
-
 struct Network {
-    static func getRequest(round: Int, completion: @escaping (Lotto) -> Void) {
+    static func getRequest(round: Int, completion: @escaping (LottoDTO) -> Void) {
         AF.request("\(APIURL.lottoURL)\(round)")
-            .responseDecodable(of: Lotto.self) { response in
+            .responseDecodable(of: LottoDTO.self) { response in
                 switch response.result {
                 case .success(let value):
                     completion(value)
