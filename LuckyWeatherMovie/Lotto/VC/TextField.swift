419441556ee1e42b0d01234cdbc4663da335e954
yoruck2 <yoruck2@gmail.com>
2024-06-09 16:38:48 +0900

    LottoViewController의 TextFieldDelegate 분리

--- LuckyWeatherMovie/Lotto/VC/TextField.swift
+++ LuckyWeatherMovie/Lotto/VC/TextField.swift
@@ -5,4 +5,22 @@
 //  Created by dopamint on 6/9/24.
 //
 
-import Foundation
+import UIKit
+
+extension LottoViewController: UITextFieldDelegate {
+
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        guard let text = textField.text, let round = Int(text) else {
+            return
+        }
+        guard round >= 0 else {
+            return
+        }
+        
+        Network.getRequest(round: round) { [weak self] lotto in
+            self?.lottoData = lotto
+        }
+        roundPickerView.selectRow(round - 1, inComponent: 0, animated: true)
+    }
+}
+
