7f3907e93609f3a7d3995c9cbd1f3dbfd852ee2b
yoruck2 <yoruck2@gmail.com>
2024-06-09 16:41:09 +0900

    UILabel 특정 구간 속성 변경을 위한 setFontColorRange 메서드 구현

--- LuckyWeatherMovie/extension/UILabel+.swift
+++ LuckyWeatherMovie/extension/UILabel+.swift
@@ -8,26 +8,13 @@
 import UIKit
 
 extension UILabel {
-    /* 텍스트 구간 색상 변경 */
-    func setTextColor(_ color: UIColor, range: NSRange) {
-        guard let attributedString = self.mutableAttributedString() else { return }
-        
-        attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: color, range: range)
-        self.attributedText = attributedString
-    }
     
-    /* AttributedString이 설정되어있지 않으면 생성하여 반환한다. */
-    private func mutableAttributedString() -> NSMutableAttributedString? {
-        guard let labelText = self.text, let labelFont = self.font else { return nil }
-        
-        var attributedString: NSMutableAttributedString?
-        if let attributedText = self.attributedText {
-            attributedString = attributedText.mutableCopy() as? NSMutableAttributedString
-        } else {
-            attributedString = NSMutableAttributedString(string: labelText,
-                                                         attributes: [NSAttributedString.Key.font :labelFont])
-        }
-        
-        return attributedString
+    func setFontColorRange(targetString: String, font: UIFont?, color: UIColor?) {
+        let fullText = text ?? ""
+        let attributedString = NSMutableAttributedString(string: fullText)
+        let range = (fullText as NSString).range(of: targetString)
+        attributedString.addAttributes([.font: font as Any, .foregroundColor: color as Any], range: range)
+        attributedText = attributedString
     }
 }
+
