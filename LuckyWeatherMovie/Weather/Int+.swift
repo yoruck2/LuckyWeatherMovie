85171b15e1147c28467577c348db9b41a9792c6d
yoruck2 <yoruck2@gmail.com>
2024-06-10 08:08:47 +0900

    배경 애니메이션을 위한 UIImage 익스텐션 추가

--- LuckyWeatherMovie/Weather/Int+.swift
+++ LuckyWeatherMovie/Weather/Int+.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+extension Int {
+    
+    func formatDate() -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(self))
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone.autoupdatingCurrent
+        dateFormatter.dateFormat = "MM월 dd일 HH시 mm분"
+        let strDate = dateFormatter.string(from: date)
+        
+        return strDate
+    }
+}
--- LuckyWeatherMovie/Weather/UIImage+.swift
+++ LuckyWeatherMovie/Weather/UIImage+.swift
@@ -5,4 +5,23 @@
 //  Created by dopamint on 6/10/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    static func animatedGif(named: String, framesPerSecond: Double = 10) -> UIImage? {
+        guard let asset = NSDataAsset(name: named) else { return nil }
+        return animatedGif(from: asset.data, framesPerSecond: framesPerSecond)
+    }
+    
+    static func animatedGif(from data: Data, framesPerSecond: Double = 10) -> UIImage? {
+        guard let source = CGImageSourceCreateWithData(data as CFData, nil) else { return nil }
+        let imageCount = CGImageSourceGetCount(source)
+        var images: [UIImage] = []
+        for i in 0 ..< imageCount {
+            if let cgImage = CGImageSourceCreateImageAtIndex(source, i, nil) {
+                images.append(UIImage(cgImage: cgImage))
+            }
+        }
+        return UIImage.animatedImage(with: images, duration: Double(images.count) / framesPerSecond)
+    }
+}
